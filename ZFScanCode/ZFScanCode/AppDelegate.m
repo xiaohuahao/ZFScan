fc916b144f35036e47b2e203385c71a5751f605a
张豪(EX-ZHANGHAO012) <EX-ZHANGHAO012@pingan.com.cn>
2017-11-17 16:08:33 +0800

    ni da ye de!

--- ZFScanCode/ZFScanCode/AppDelegate.m
+++ ZFScanCode/ZFScanCode/AppDelegate.m
@@ -19,6 +19,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"你大爷的");
+    
     return YES;
 }
 
