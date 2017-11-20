574a2d93573bca961267e4e47da5a81c6787b9ba
xiaohuahao <1803660251@qq.com>
2017-11-20 16:54:32 +0800

    新增haonew

---
+++ ZFScanCode/ZFScanCode/HaoNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  HaoNewViewController.h
+//  ZFScanCode
+//
+//  Created by 张豪(EX-ZHANGHAO012) on 2017/11/20.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HaoNewViewController : UIViewController
+
+@end
